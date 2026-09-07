8936446a0fa391fbbe8347a2e4521ebd1328c95f
salmansayanidev <salmansayani822@gmail.com>
2026-09-07 19:16:35 +0500

    modifications

--- src/components/Popups/AnnoucementsPopup.tsx
+++ src/components/Popups/AnnoucementsPopup.tsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import "./AnnoucementsPopup.css";
 
-const AnnoucementsPopup = () => {
+interface  AnnoucementPopupProps {
+    isOpen: boolean;
+    onClose: () => void;
+    onPost?: (data: {
+        title: string;
+        message: string;   
+    }) => void;
+}
+
+
+const AnnoucementsPopup = ({ isOpen, onClose, onPost }: AnnoucementPopupProps) => {
+
+    const [ title, setTitle ] = useState("");
+    const [ message, setMessage] = useState("");
+
+    if (!isOpen) {
+        return null;
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault;
+
+        onPost?.({
+            title,
+            message
+        });
+
+        setTitle("");
+        setMessage("");
+        onClose();
+    }
+
     return (
         <div className="popup-wrapper">
             <div className="popup-box">
                 <h2 className="popup-title">Add New Announcement</h2>
+                <form className="m-0" onSubmit={handleSubmit}>
+                    
+                </form>
             </div>
         </div>
     );
